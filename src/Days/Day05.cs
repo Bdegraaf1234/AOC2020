bff8c8a9c26f9ca9c060dbb2eaa77566c88f5ab8
Bdegraaf1234 <bdegraaf1234@gmail.com>
2020-12-09 20:46:44 +0100

    Day5 done

--- src/Days/Day05.cs
+++ src/Days/Day05.cs
@@ -9,12 +9,89 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var boardingPasses = input.Lines();
+
+            List<int> ids = boardingPasses.Select(x => GetId(x)).ToList();
+
+			return ids.Max().ToString();
         }
 
-        public override string PartTwo(string input)
-        {
-            throw new NotImplementedException();
+		public int GetId(string input)
+		{
+            int row = GetRow(input);
+            int column = GetColumn(input);
+
+            return (row * 8) + column;
         }
+
+		private int GetRow(string input)
+		{
+            var idxs = Enumerable.Range(0, 128);
+
+            var relevantChars = input.ToCharArray().Take(7).ToArray();
+			for (int i = 0; i < relevantChars.Count(); i++)
+			{
+				if (relevantChars[i] == 'F')
+					idxs = TakeBottomHalf(idxs);
+				else
+					idxs = TakeTopHalf(idxs);
+			}
+
+			if (idxs.Count() != 1)
+				throw new Exception("Too many values");
+
+			return idxs.First();
+		}
+
+		private IEnumerable<int> TakeTopHalf(IEnumerable<int> idxs)
+		{
+			int numToTake= idxs.Count() / 2;
+			return idxs.Skip(numToTake).Take(numToTake).ToArray();
+		}
+
+		private IEnumerable<int> TakeBottomHalf(IEnumerable<int> idxs)
+		{
+			int numToTake = idxs.Count() / 2;
+			return idxs.Take(numToTake).ToArray();
+		}
+
+		private int GetColumn(string input)
+		{
+			var idxs = Enumerable.Range(0, 8);
+
+			var relevantChars = input.ToCharArray().Skip(7).Take(3).ToArray();
+			for (int i = 0; i < relevantChars.Count(); i++)
+			{
+				if (relevantChars[i] == 'L')
+					idxs = TakeBottomHalf(idxs);
+				else
+					idxs = TakeTopHalf(idxs);
+			}
+
+			if (idxs.Count() != 1)
+				throw new Exception("Too many values");
+
+			return idxs.First();
+		}
+
+		public override string PartTwo(string input)
+        {
+			var boardingPasses = input.Lines();
+
+			List<int> ids = boardingPasses.Select(x => GetId(x)).ToList();
+
+			ids.Sort();
+			int prevId = 0;
+			for (int i = 0; i < ids.Count; i++)
+			{
+				if (prevId == ids[i] - 2)
+				{
+					return (ids[i] - 1).ToString();
+				}
+				prevId = ids[i];
+			}
+			
+			throw new Exception("No suitable solution found");
+		}
     }
 }
---
+++ tests/AOC2020Tests/Day6Tests.cs
@@ -0,0 +1,46 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode.Days;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Reflection;
+
+namespace AOC2020Tests
+{
+	[TestClass]
+	public class Day6Tests
+	{
+		private const int dayNum = 5;
+		private AdventOfCode.BaseDay ThisDay = new Day05();
+
+		[DataTestMethod]
+		public void Part1Test1()
+		{
+			string input = "BFFFBBFRRR";
+			var ans = ThisDay.PartOne(input);
+
+			Assert.AreEqual("567", ans);
+		}
+
+	
+		[DataTestMethod]
+		public void Part1Test2()
+		{
+			string input = File.ReadAllText($@"C:\Users\3765563\source\repos\AOC2020\tests\AOC2020Tests\input\day{dayNum}\Day{dayNum}Input.txt");
+
+			var ans = ThisDay.PartOne(input);
+
+			Assert.AreEqual("987", ans);
+		}
+
+		[DataTestMethod]
+		public void Part2Test2()
+		{
+			string input = File.ReadAllText($@"C:\Users\3765563\source\repos\AOC2020\tests\AOC2020Tests\input\day{dayNum}\Day{dayNum}Input.txt");
+
+			var ans = ThisDay.PartTwo(input);
+
+			Assert.AreEqual("603", ans);
+		}
+	}
+}
