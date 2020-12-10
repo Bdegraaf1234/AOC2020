ce463dbae8eb46008046e826bca15b7e00d3f974
Bdegraaf1234 <bdegraaf1234@gmail.com>
2020-12-10 19:53:07 +0100

    day9 complete

--- src/Days/Day09.cs
+++ src/Days/Day09.cs
@@ -9,12 +9,47 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var inputNums = input.Longs().ToList();
+
+            int preamble = 25;
+
+            var idxCombos = MyHelpers.GenerateCombinations(2, 0, preamble - 1);
+
+			for (int i = preamble; i < inputNums.Count(); i++)
+			{
+                long curNum = inputNums[i];
+                var combos = idxCombos.Select(x => inputNums[x[0] + i - preamble] + inputNums[x[1] + i - preamble]);
+				if (!combos.Contains(curNum))
+				{
+                    return curNum.ToString();
+				}
+			}
+
+            return "";
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            var inputNums = input.Longs().ToList();
+
+            int count = inputNums.Count;
+
+            var idxCombos = MyHelpers.GenerateCombinations(2, 0, count - 1);
+
+            var res = new Dictionary<int[], long>();
+			foreach (var idxCombo in idxCombos)
+			{
+				if (idxCombo[0] - idxCombo[1] < -1)
+				{
+					IEnumerable<int> idxs = Enumerable.Range(idxCombo[0], idxCombo[1] - idxCombo[0]);
+					res.Add(idxCombo, idxs.Select(x => inputNums[x]).Sum());
+				}
+			}
+
+            var ans = res.Where(x => x.Value == 177777905).First().Key;
+
+			IEnumerable<long> ans2 = Enumerable.Range(ans[0], ans[1] - ans[0]).Select(x => inputNums[x]);
+			return (ans2.Min() + ans2.Max()).ToString();
         }
     }
 }
---
+++ tests/AOC2020Tests/Day9Tests.cs
@@ -0,0 +1,58 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode.Days;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Reflection;
+
+namespace AOC2020Tests
+{
+	[TestClass]
+	public class Day9Tests
+	{
+		private const int dayNum = 9;
+		private AdventOfCode.BaseDay ThisDay = new Day09();
+
+		[DataTestMethod]
+		public void Part1Test1()
+		{
+			string input = File.ReadAllText($@"C:\Users\3765563\source\repos\AOC2020\tests\AOC2020Tests\input\day{dayNum}\Day{dayNum}Example.txt");
+
+			var ans = ThisDay.PartOne(input);
+
+			Assert.AreEqual("127", ans);
+		}
+
+	
+		[DataTestMethod]
+		public void Part1Test2()
+		{
+			string input = File.ReadAllText($@"C:\Users\3765563\source\repos\AOC2020\tests\AOC2020Tests\input\day{dayNum}\Day{dayNum}Input.txt");
+
+			var ans = ThisDay.PartOne(input);
+
+			Assert.AreEqual("177777905", ans);
+		}
+
+		[DataTestMethod]
+		public void Part2Test1()
+		{
+			string input = File.ReadAllText($@"C:\Users\3765563\source\repos\AOC2020\tests\AOC2020Tests\input\day{dayNum}\Day{dayNum}Example.txt");
+
+			var ans = ThisDay.PartTwo(input);
+
+			Assert.AreEqual("62", ans);
+		}
+
+
+		[DataTestMethod]
+		public void Part2Test3()
+		{
+			string input = File.ReadAllText($@"C:\Users\3765563\source\repos\AOC2020\tests\AOC2020Tests\input\day{dayNum}\Day{dayNum}Input.txt");
+
+			var ans = ThisDay.PartTwo(input);
+
+			Assert.AreEqual("23463012", ans);
+		}
+	}
+}
